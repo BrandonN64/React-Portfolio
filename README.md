cbae5f392a80b7aa29a1549a1b60ed0b0f6696a4
BrandonN64 <Brandon.nastri@gmail.com>
2022-12-19 16:39:57 -0500

    uploading of my react portfolio

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # React-Portfolio
+
+This portfolio was made using react and a template that had some basic components compiled for me.
+
+
+This was made using HTML and Javascript.
---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+    "imagebaseurl":"http://github.com/BrandonN64",
+    "name": "Brandon Nastri",
+    "role": "Student",
+    "linkedinId":" https://www.linkedin.com/in/brandon-nastri-664571242/",
+    "skypeid": "",
+    "roleDescription": " I am currently going for my bachelors in computer science from Southern New Hampshire University while doing other classes to further my knowledge.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/brandon-nastri-664571242/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/BrandonN64",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/Brandon64N",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently looking for a job as a junior software dev or junior fullstack web developer to increase my skills that I learned during a FullStack Web Dev Bootcamp.",
+    "address":"Marlborough, Connecticut",
+    "website":"http://github.com/BrandonN64",
+    "education":[
+      {
+        "UniversityName":"Southern New Hampshire University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Big Y",
+        "specialization":"Overnight Crew Clerk",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2022",
+        "Achievements":""
+      },
+    ],
+    "skillsDescription":"Programming Knowledge",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MYSQL"
+      },
+      {
+        "skillname":"NodeJS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"BluFlix",
+        "description":"A website that was made to show random IMDB Movies",
+        "imgurl":"images/bluflix.png"
+      },
+      {
+        "name":"Flutter",
+        "description":"A online social media site",
+        "imgurl":"images/flutter.png"
+      },
+      {
+        "name":"Tough Decisions",
+        "description":"An online multiplayer game",  
+        "imgurl":"images/toughdecisions.png"
+      },
+    ],
+  }
+  
+  export default resumeData
